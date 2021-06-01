7d7018f54d2dd279f3dde2203c891631c79ba5c5
unknown <cuongnvc5802@gmail.com>
2021-06-01 14:06:01 +0700

    Encapsulation

--- src/lesson25/LyThuyet/Run.java
+++
@@ -1,9 +0,0 @@
-package lesson25.LyThuyet;
-
-public class Run {
-    public static void main(String[] args) {
-        //lớp abstract không thể tạo đối tượng
-        Person cuong = new Student();
-        cuong.earnMoney();
-    }
-}
